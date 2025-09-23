bcfcfe42a5ddee2e15376f15ee4a53517eca3bbe
StefanosSt <sstamoulis.wd@gmail.com>
2025-09-23 15:23:16 +0300

    Add the 404 page
    I could use the errorElement here but I needed to reapply all the layout again.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,14 @@
+import catNotFound from '@assets/404.png';
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
+   return (
+        <img
+            src={catNotFound}
+            className="pnf-image"
+            alt={'Page Not Found'}
+            loading="lazy"
+        />
+    );
 }
 
 export default NotFound
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -25,7 +25,7 @@ export const router = createBrowserRouter([
       },
       {
         path: '*',
-        element: <NotFound />,
+        element: <NotFound />, // Could use the errorElement property but needed to reapply layout components
       },
     ],
   },
